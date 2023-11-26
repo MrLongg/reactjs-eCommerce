85d936375ab25773665c5418aad34c431125d07a
Long <trandohoanglong113@gmail>
2023-11-26 16:09:04 +0700

    Xong API

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 router.post('/register', async (req, res) => {
@@ -32,9 +33,15 @@ router.post('/login', async (req, res) => {
         if (OriginalPassword !== req.body.password) {
             return res.status(401).json('Wrong credentials!');
         }
+
+        const accessToken = jwt.sign({
+            id: user._id, 
+            isAdmin: user.isAdmin, 
+        }, process.env.JWT_SEC, {expiresIn: "3d"})
+
         const { password, ...others } = user._doc
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
